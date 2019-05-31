2a10c6d5bf7e492da45b8578820402a8301d761b
nome_usuariorafael_brigidocdgit config --global user.email email_usuariorafaelgrohl222@gmail.com <rafaelgrohl222@gmail.com>
2019-05-31 15:13:52 -0400

    C#.Net - Cursando - Primeiros Passos

---
+++ 6-AtribuicoesDeVariavel/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtribuicoesDeVariavel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o Projeto 6: Atribuições de Variáveis");
+
+            Console.WriteLine();//espaço
+
+            int idade = 32;
+            int idadeGustavo = idade;
+
+            idade = 20;
+
+            Console.WriteLine("Idade: " + idade);//idade: 20
+            Console.WriteLine("Idade Gustavo: " + idadeGustavo);//idade gustavo: 32
+
+            Console.WriteLine();//espaço
+
+            Console.WriteLine("Acabou a execução. Aperte o ENTER para SAIR...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 7-Condicionais_if/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Condicionais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o Projeto 7: Condicionais IF");
+
+            Console.WriteLine();//espaço
+
+            int idadeJoao = 16;
+            int quantidadePessoa = 2;
+
+            //Início do Bloco de Condições
+            if (idadeJoao >= 18)
+            {
+                Console.WriteLine("Você possui 18 anos, você pode entrar!" + "Sua Idade: " + idadeJoao);
+            }//fim se
+            else
+            {
+                if (quantidadePessoa >= 2)
+                {
+                    Console.WriteLine("Você não possue 18 anos! Mas está acompanhado! PODE ENTRAR!" + "Sua Idade: " + idadeJoao);
+                }
+                else
+                {
+                    Console.WriteLine("Você NÃO possui 18 anos! Então NÃO pode entrar!" + "Sua Idade: " + idadeJoao);
+                }
+            }//fim condição else
+            Console.WriteLine();//espaço
+
+            Console.WriteLine("A Execução do Projeto Finalizou, aperte ENTER para SAIR...");
+            Console.ReadLine();//Aguardando o comando ENTER para sair do projeto
+        }//6min40seg
+    }
+}
