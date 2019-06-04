a9b0b950ab9ffa18c973a60654b1144b141371e9
nome_usuariorafael_brigidocdgit config --global user.email email_usuariorafaelgrohl222@gmail.com <rafaelgrohl222@gmail.com>
2019-06-04 16:58:29 -0400

    C # .Net - Cursando - Primeiros Passos

---
+++ 7-Condicionais_if/Program (2).cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Condicionais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o Projeto 7: Condicionais IF");
+
+            Console.WriteLine();//espaço
+
+            int idadeJoao = 16;
+            int quantidadePessoa = 2;
+
+            //Início do Bloco de Condições
+            if (idadeJoao >= 18)
+            {
+                Console.WriteLine("Você tem mais que 18 anos! " + "Sua Idade: " + idadeJoao);
+                Console.WriteLine("Seja bem vindo");
+            }//fim se
+            else
+            {
+                if (quantidadePessoa >= 2)
+                {
+                    Console.WriteLine("Você não possue 18 anos! Mas está acompanhado! PODE ENTRAR!" + "Sua Idade: " + idadeJoao);
+                    Console.WriteLine("Seja bem vindo");
+                }
+                else
+                {
+                    Console.WriteLine("infelizmente NÃO possui 18 anos e NÃO esta acompanhado! NÃO pode entrar !" + "Sua Idade: " + idadeJoao);
+                }
+            }//fim condição else
+            Console.WriteLine();//espaço
+
+            Console.WriteLine("A Execução do Projeto Finalizou, aperte ENTER para SAIR...");
+            Console.ReadLine();//Aguardando o comando ENTER para sair do projeto
+        }//6min40seg
+    }
+}
---
+++ 8-Condicionais_booleana/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Condicionais_booleana
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o Projeto 8: Condicionais Booleana");
+
+            Console.WriteLine();//espaço
+
+            int idadeJoao = 18;
+            //int quantidadePessoa = 2;
+            //bool acompanhado = quantidadePessoa >= 2;
+            bool acompanhado = true;
+
+            //se(idadeJoao for maior igual a 18 OU / || quantidadePessoa for mair igual a 2
+            //if (idadeJoao >= 18 && quantidadePessoa >= 2)
+            //=Tipos: ou = ||, e = &&, AND = , OR = .
+            //=Tipo: true ou fause
+
+            //Início do Bloco de Condições
+            if (idadeJoao >= 18 && acompanhado == true)
+            {
+                Console.WriteLine("Você pode entrar!");
+                Console.WriteLine("Seja bem Vindo!");
+            }//fim se
+            else
+            {
+                Console.WriteLine("Você NÃO pode entrar!");
+            }//fim condição else
+
+            /*
+            if (idadeJoao >= 18 && quantidadePessoa >= 2)
+            {
+                Console.WriteLine("Você pode entrar!");
+                Console.WriteLine("Seja bem Vindo!");
+            }//fim se
+            else
+            {
+                Console.WriteLine("Você NÃO pode entrar!");
+            }//fim condição else
+            */
+            int idade = 68;
+            bool ehIdoso = idade >= 65;
+            Console.WriteLine(ehIdoso);
+
+            Console.WriteLine();//espaço
+
+            Console.WriteLine("A Execução do Projeto Finalizou, aperte ENTER para SAIR...");
+            Console.ReadLine();//Aguardando o comando ENTER para sair do projeto
+        }
+    }
+}
