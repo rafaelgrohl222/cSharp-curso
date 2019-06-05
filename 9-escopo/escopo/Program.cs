a0851aeed50650b77b9b5607451646e69af7069d
nome_usuariorafael_brigidocdgit config --global user.email email_usuariorafaelgrohl222@gmail.com <rafaelgrohl222@gmail.com>
2019-06-05 17:00:19 -0400

    C # .Net - Cursando - Primeiros Passos

---
+++ 9-escopo/escopo/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace escopo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o Projeto 9: Escopo");
+
+            Console.WriteLine();//espaço
+
+            int idadeJoao = 18;
+            bool acompanhado = false;
+            String mensagemAdicional;
+
+            if (acompanhado == true)
+            {
+                mensagemAdicional = "João ESTÁ acompanhado";
+            }
+            else
+            {
+                mensagemAdicional = "João Não está acompanhado";
+            }
+
+            //Início do Bloco de Condições
+            if (idadeJoao >= 18 && acompanhado == true)
+            {
+                Console.WriteLine("Você pode entrar!");
+                Console.WriteLine(mensagemAdicional);
+            }//fim se
+            else
+            {
+                Console.WriteLine("Você NÃO pode entrar!");
+                Console.WriteLine("Só pode entrar! Com Maior de 18 ou estando ACOMPANHADO");
+            }//fim condição else
+
+            Console.WriteLine();//espaço
+
+            Console.WriteLine("A Execução do Projeto Finalizou, aperte ENTER para SAIR...");
+            Console.ReadLine();//Aguardando o comando ENTER para sair do projeto
+        }
+    }
+}
