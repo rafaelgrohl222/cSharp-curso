a4bd5ac6d454b0c221c787a0d7cd1d4e5914c2a6
nome_usuariorafael_brigidocdgit config --global user.email email_usuariorafaelgrohl222@gmail.com <rafaelgrohl222@gmail.com>
2019-06-14 16:58:52 -0400

    C # .Net - Cursando - Primeiros Passos

---
+++ p11-cauculaPoupanca2-for/p11-cauculaPoupanca2-for/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p11_cauculaPoupanca2_for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 11: Laço de repetição For");
+            Console.WriteLine();//espaço
+
+            double valorInvestido = 1000;
+
+            //for = laço para
+            for (int contadorMes = 1; contadorMes <= 12; contadorMes++)
+            {
+                valorInvestido = valorInvestido * 1.0036;
+                Console.WriteLine("Após " + contadorMes + " meses, você terá $" + valorInvestido);
+            }
+
+            Console.ReadLine();//Aguardando o clique no enter para sair.
+        }
+    }
+}
